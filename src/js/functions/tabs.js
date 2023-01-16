d6671a7b962db7d11eb53ec852006d30d27bf5e5
karnauhmax <krnhmx@gmail.com>
2023-01-16 18:19:38 +0300

    minor fixes

--- src/js/functions/tabs.js
+++ src/js/functions/tabs.js
@@ -8,28 +8,24 @@ class Tab {
   renderTab() {
     this.tabContainer.addEventListener("click", (e) => {
       if (e.target.classList.contains("tabs__btn")) {
-        const self = e.target;
-        const tabsPath = self.dataset.tabsPath;
+        let self = e.target;
+        let tabsPath = e.target.dataset.tabsPath;
+
         const tabsHandler = (path) => {
-          this.tabBtn.forEach((button) => {
-            button.classList.remove("tabs__btn-active");
+          this.tabBtn.forEach((btn) => {
+            btn.classList.remove("tabs__btn-active");
           });
 
-          if (path) {
-            document
-              .querySelector(`[data-tabs-path=${path}]`)
-              .classList.add("tabs__btn-active");
-          }
-
-          this.tabContent.forEach((content) => {
-            content.classList.remove("tabs__content-active");
+          this.tabContent.forEach((btn) => {
+            btn.classList.remove("tabs__content-active");
           });
 
-          if (path) {
-            document
-              .querySelector(`[data-tabs-target=${path}]`)
-              .classList.add("tabs__content-active");
-          }
+          this.tabContainer
+            .querySelector(`[data-tabs-path=${path}]`)
+            .classList.add("tabs__btn-active");
+          this.tabContainer
+            .querySelector(`[data-tabs-target=${path}]`)
+            .classList.add("tabs__content-active");
         };
 
         tabsHandler(tabsPath);
