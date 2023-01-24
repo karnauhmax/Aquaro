4116c527ba418c2542ae75c471a8fbac746cbe3f
karnauhmax <krnhmx@gmail.com>
2023-01-24 22:01:28 +0300

    header logic on another pages

--- src/js/sections/headerMenu.js
+++ src/js/sections/headerMenu.js
@@ -1,25 +1,28 @@
-const menu = document.querySelector(".menu");
-const menuLinks = document.querySelectorAll("[data-goto]");
-const header = document.querySelector(".header");
+const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('[data-goto]');
+const header = document.querySelector('.header');
 
 //opening sub menu by click
 if (menu) {
-  document.addEventListener("click", (e) => {
+  document.addEventListener('click', e => {
     const self = e.target;
-    const subMenu = self.parentElement.querySelector(".sub-menu");
-    if (self.closest(".menu__item")) {
+    const subMenu = self.parentElement.querySelector('.sub-menu');
+    if (
+      self.closest('.menu__item') &&
+      self.closest('.menu__item').querySelector('.sub-menu')
+    ) {
       e.preventDefault();
-      const item = self.closest(".menu__item");
+      const item = self.closest('.menu__item');
 
       if (subMenu) {
-        item.classList.toggle("sub-menu__active");
+        item.classList.toggle('sub-menu__active');
       }
     } else {
-      if (document.querySelector(".sub-menu__active")) {
+      if (document.querySelector('.sub-menu__active')) {
         const subMenusCollection =
-          document.querySelectorAll(".sub-menu__active");
-        subMenusCollection.forEach((item) => {
-          item.classList.remove("sub-menu__active");
+          document.querySelectorAll('.sub-menu__active');
+        subMenusCollection.forEach(item => {
+          item.classList.remove('sub-menu__active');
         });
       }
     }
@@ -29,15 +32,15 @@ if (menu) {
 //scroll to links
 
 if (menuLinks.length > 0) {
-  menuLinks.forEach((menuLinks) => {
-    menuLinks.addEventListener("click", (e) => {
+  menuLinks.forEach(menuLinks => {
+    menuLinks.addEventListener('click', e => {
       const menuLink = e.target;
-      if (menu.classList.contains("menu--active")) {
-        menu.classList.remove("menu--active");
-        document.body.classList.remove("dis-scroll");
+      if (menu.classList.contains('menu--active')) {
+        menu.classList.remove('menu--active');
+        document.body.classList.remove('dis-scroll');
         document
-          .querySelector(".header__burger")
-          .classList.remove("burger--active");
+          .querySelector('.header__burger')
+          .classList.remove('burger--active');
       }
       if (
         menuLink.dataset.goto &&
@@ -47,11 +50,11 @@ if (menuLinks.length > 0) {
         const gotoBlockValue =
           gotoBlock.getBoundingClientRect().top +
           pageYOffset -
-          document.querySelector(".header").offsetHeight;
+          document.querySelector('.header').offsetHeight;
 
         window.scrollTo({
           top: gotoBlockValue,
-          behavior: "smooth",
+          behavior: 'smooth'
         });
 
         e.preventDefault();
@@ -64,9 +67,9 @@ if (menuLinks.length > 0) {
 
 if (header) {
   const menuObserver = new IntersectionObserver((entries, observer) => {
-    entries.forEach((entry) => {
+    entries.forEach(entry => {
       if (entry.intersectionRatio <= 0.1) {
-        entry.target.classList.add("fixed");
+        entry.target.classList.add('fixed');
       }
     });
   }, {});
@@ -76,7 +79,7 @@ if (header) {
 
 //cutting cart amount
 
-let amount = document.querySelector(".cart__amount").textContent;
+let amount = document.querySelector('.cart__amount').textContent;
 
 if (amount.trim().length > 2) {
   const cutted = `${amount.textContent.substring(0, 2)}+`;
